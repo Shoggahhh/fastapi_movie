e0177f53e4ad50bb90348102cdf14361be7f5e4a
vasiliy_grinev <vass_stn@mail.ru>
2025-07-24 17:58:53 +0300

    update main

--- movie/main.py
+++ movie/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI, Request
+from api import router as api_router
+from api.redirect_views import router as redirect_router
 
 app = FastAPI()
+
+app.include_router(redirect_router)
+app.include_router(api_router)
 
 
 @app.get("/")
