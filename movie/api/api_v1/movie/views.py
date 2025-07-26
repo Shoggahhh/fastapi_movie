1dbd3bcce947f6e76e6c0d8614dcf753454d35b4
vasiliy_grinev <vass_stn@mail.ru>
2025-07-26 13:18:13 +0300

    add new router post create movie

--- movie/api/api_v1/movie/views.py
+++ movie/api/api_v1/movie/views.py
@@ -1,10 +1,13 @@
-from fastapi import APIRouter
-from fastapi.params import Depends
+from fastapi import APIRouter, status, Form, Depends
+from pydantic import AnyHttpUrl
 
 from api.api_v1.movie.dependecies import prefetch_movie
 from api.api_v1.movie.crud import MOVIES
 from movie.schemas.movie import Movie
 from typing import Annotated
+from annotated_types import Len
+
+import random
 
 router = APIRouter(prefix="/movies", tags=["Movies"])
 
@@ -12,6 +15,28 @@ router = APIRouter(prefix="/movies", tags=["Movies"])
 @router.get("/", response_model=list[Movie])
 def get_all_movies():
     return MOVIES
+
+
+@router.post(
+    "/",
+    response_model=Movie,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_movie(
+    name: Annotated[str, Len(min_length=1, max_length=59), Form()],
+    description: Annotated[str, Len(min_length=5, max_length=100), Form()],
+    rating: Annotated[str, Len(min_length=1, max_length=2), Form()],
+    age_rating: Annotated[str, Len(min_length=2, max_length=3), Form()],
+    url: Annotated[AnyHttpUrl, Form()],
+):
+    return Movie(
+        movie_id=random.randint(3, 10),
+        description=description,
+        name=name,
+        rating=rating,
+        age_rating=age_rating,
+        url=url,
+    )
 
 
 @router.get("/movie/{movie_id}", response_model=Movie)
