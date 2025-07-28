eaa4a3c2c4f6e3d5c1698d617680d07ac7e2840f
vasiliy_grinev <vass_stn@mail.ru>
2025-07-28 17:17:22 +0300

    update dependencies.py

--- movie/api/api_v1/movie/dependencies.py
+++ movie/api/api_v1/movie/dependencies.py
@@ -1,18 +1,16 @@
 from fastapi import HTTPException
 from starlette import status
 
-from api.api_v1.movie.crud import MOVIES
+from api.api_v1.movie.crud import storage
 from schemas.movie import Movie
 
 
 def prefetch_movie(slug: str):
-    movie: Movie | None = next(
-        (movie for movie in MOVIES if movie.slug == slug),
-        None,
-    )
+    movie: Movie | None = storage.get_by_slug(slug=slug)
+
     if movie:
         return movie
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
-        detail=f"Movie on this id: {slug!r} not found",
+        detail=f"Movie on this slug: {slug!r} not found",
     )
