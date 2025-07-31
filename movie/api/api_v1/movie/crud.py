cbe3dbaef9a0e0c77af4ce732da6ef728e146b2c
vasiliy_grinev <vass_stn@mail.ru>
2025-07-31 14:45:17 +0300

    add delete to storage

--- movie/api/api_v1/movie/crud.py
+++ movie/api/api_v1/movie/crud.py
@@ -28,6 +28,12 @@ class MovieStorage(BaseModel):
         self.slug_to_movie[movie_in.slug] = movie
         return movie
 
+    def delete_by_slug(self, slug: str) -> None:
+        self.slug_to_movie.pop(slug, None)
+
+    def delete(self, movie: Movie) -> None:
+        self.delete_by_slug(slug=movie.slug)
+
 
 storage = MovieStorage()
 
