06878d01657ca76ee66399d3bc46e3bfdae9ff43
vasiliy_grinev <vass_stn@mail.ru>
2025-07-31 16:09:32 +0300

    moved from views.py to list_views.py

---
+++ movie/api/api_v1/movie/views/list_views.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, status
+
+from api.api_v1.movie.crud import storage
+from movie.schemas.movie import Movie, MovieCreate
+
+router = APIRouter(prefix="/movies", tags=["Movies"])
+
+
+@router.get("/", response_model=list[Movie])
+def read_all_movies():
+    return storage.get()
+
+
+@router.post(
+    "/",
+    response_model=Movie,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_movie(movie_create: MovieCreate) -> Movie:
+    return storage.create(movie_create)
