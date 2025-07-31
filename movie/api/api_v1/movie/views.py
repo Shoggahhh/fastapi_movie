a85065b0c3dcc7e0e05fa1657fb90bd50a83025b
vasiliy_grinev <vass_stn@mail.ru>
2025-07-31 14:45:41 +0300

    add delete router

--- movie/api/api_v1/movie/views.py
+++ movie/api/api_v1/movie/views.py
@@ -25,5 +25,29 @@ def create_movie(movie_create: MovieCreate) -> Movie:
 
 
 @router.get("/movie/{slug}", response_model=Movie)
-def get_movie_from_id(movie: Annotated[Movie, Depends(prefetch_movie)]) -> Movie:
+def get_movie_from_slug(
+    movie: Annotated[Movie, Depends(prefetch_movie)],
+) -> Movie:
     return movie
+
+
+@router.delete(
+    "movie/{slug}/",
+    status_code=status.HTTP_204_NO_CONTENT,
+    responses={
+        status.HTTP_404_NOT_FOUND: {
+            "description": "Movie not found",
+            "content": {
+                "application/json": {
+                    "example": {
+                        "detail": "Movie on this 'slug' not found",
+                    },
+                },
+            },
+        },
+    },
+)
+def delete_movie(
+    movie: Annotated[Movie, Depends(prefetch_movie)],
+) -> None:
+    storage.delete(movie=movie)
