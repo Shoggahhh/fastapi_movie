8975f6f7c02014b0bf9eb5905bc6abfa72b2fe61
vasiliy_grinev <vass_stn@mail.ru>
2025-08-04 14:22:09 +0300

    update to MovieRead instead Movie schemas

--- movie/api/api_v1/movie/views/list_views.py
+++ movie/api/api_v1/movie/views/list_views.py
@@ -1,19 +1,23 @@
 from fastapi import APIRouter, status
 
 from api.api_v1.movie.crud import storage
-from movie.schemas.movie import Movie, MovieCreate
+from movie.schemas.movie import (
+    Movie,
+    MovieCreate,
+    MovieRead,
+)
 
 router = APIRouter(prefix="/movies", tags=["Movies"])
 
 
-@router.get("/", response_model=list[Movie])
+@router.get("/", response_model=list[MovieRead])
 def read_all_movies():
     return storage.get()
 
 
 @router.post(
     "/",
-    response_model=Movie,
+    response_model=MovieRead,
     status_code=status.HTTP_201_CREATED,
 )
 def create_movie(movie_create: MovieCreate) -> Movie:
