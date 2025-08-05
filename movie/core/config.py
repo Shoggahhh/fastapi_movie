edc11e626596e97d192b4de933ba475cc58b0a94
vasiliy_grinev <vass_stn@mail.ru>
2025-08-05 11:03:18 +0300

    add logging constant

--- movie/core/config.py
+++ movie/core/config.py
@@ -1,4 +1,11 @@
+import logging
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 MOVIE_STORAGE_DIR = BASE_DIR / "movie.json"
+
+
+LOG_LEVEL = logging.INFO
+LOG_FORMAT: str = (
+    "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
+)
