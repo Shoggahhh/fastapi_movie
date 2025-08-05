0c8b05f35ddce6e143f1a23225dfcf1430645f36
vasiliy_grinev <vass_stn@mail.ru>
2025-08-05 11:03:36 +0300

    add logging basic config

--- movie/main.py
+++ movie/main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI, Request
 from api import router as api_router
 from api.redirect_views import router as redirect_router
+import logging
+from movie.core import config
 
 app = FastAPI()
 
 app.include_router(redirect_router)
 app.include_router(api_router)
+
+logging.basicConfig(
+    level=config.LOG_LEVEL,
+    format=config.LOG_FORMAT,
+)
 
 
 @app.get("/")
