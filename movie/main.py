cb3cb32e6ae074cbfa81a2bca9178a716e1b2767
vasiliy_grinev <vass_stn@mail.ru>
2025-08-05 11:49:31 +0300

    add lifespan to main in app

--- movie/main.py
+++ movie/main.py
@@ -2,9 +2,14 @@ from fastapi import FastAPI, Request
 from api import router as api_router
 from api.redirect_views import router as redirect_router
 import logging
+
+from app_lifespan import lifespan
 from movie.core import config
 
-app = FastAPI()
+app = FastAPI(
+    title="Movie",
+    lifespan=lifespan,
+)
 
 app.include_router(redirect_router)
 app.include_router(api_router)
