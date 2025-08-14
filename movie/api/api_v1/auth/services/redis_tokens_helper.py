50e431c4c4806d963384c4dc5f89f962f024ed34
vasiliy_grinev <vass_stn@mail.ru>
2025-08-14 15:10:10 +0300

    moved to redis_tokens_helper.py and tokens_helper.py

--- movie/api/api_v1/auth/services/redis_tokens_helper.py
+++ movie/api/api_v1/auth/services/redis_tokens_helper.py
@@ -1,36 +1,7 @@
-import secrets
-
 from redis import Redis
+
+from api.api_v1.auth.services.tokens_helper import AbstractTokensHelper
 from core import config
-
-from abc import ABC, abstractmethod
-
-
-class AbstractTokensHelper(ABC):
-    @abstractmethod
-    def token_exist(self, token: str) -> bool:
-        """
-        Check if tokens exist
-        :param token:
-        :return:
-        """
-
-    @abstractmethod
-    def add_token(self, token: str) -> None:
-        """
-        Save token in storage
-        :param token:
-        :return:
-        """
-
-    @classmethod
-    def generate_token(cls) -> str:
-        return secrets.token_urlsafe(16)
-
-    def generate_and_save_token(self) -> str:
-        token = self.generate_token()
-        self.add_token(token)
-        return token
 
 
 class RedisTokensHelper(AbstractTokensHelper):
