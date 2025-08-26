ad0a413c39a426ca845206bc87b1d1f7480f1aef
vasiliy_grinev <vass_stn@mail.ru>
2025-08-26 16:20:06 +0300

    update token cli

--- movie/commands/tokens.py
+++ movie/commands/tokens.py
@@ -5,6 +5,7 @@ from typing import Annotated
 from rich.markdown import Markdown
 
 from api.api_v1.auth.services import redis_tokens
+import secrets
 
 app = typer.Typer(
     name="token",
@@ -42,3 +43,39 @@ def list_tokens():
     print(Markdown("# Available API Tokens"))
     print(Markdown("\n- ".join([""] + redis_tokens.get_tokens())))
     print()
+
+
+@app.command(name="create")
+def create_token():
+    """
+    Create token
+    """
+    print(secrets.token_urlsafe(16))
+
+
+@app.command(name="add")
+def add_token(
+    token: Annotated[
+        str,
+        typer.Argument(help="Token to add"),
+    ],
+):
+    """
+    Add token
+    """
+    redis_tokens.add_token(token)
+    print(f"Token [bold]{token}[/bold] [bold green]added[/bold green].")
+
+
+@app.command(name="delete")
+def delete_token(
+    token: Annotated[
+        str,
+        typer.Argument(help="Token to delete"),
+    ],
+):
+    """
+    Delete token
+    """
+    redis_tokens.delete_token(token)
+    print(f"Token [bold]{token}[/bold] [bold green]deleted[/bold green].")
