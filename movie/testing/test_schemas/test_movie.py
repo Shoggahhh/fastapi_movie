221ae77e2aa13758ce7f43818ed3ab0cbb4ce5b1
vasiliy_grinev <vass_stn@mail.ru>
2025-08-31 14:44:17 +0300

    add new tests

--- movie/testing/test_schemas/test_movie.py
+++ movie/testing/test_schemas/test_movie.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from schemas.movie import Movie, MovieCreate
+from schemas.movie import Movie, MovieCreate, MoviePartialUpdate, MovieUpdate
 
 
 class MovieTestCase(TestCase):
@@ -25,3 +25,55 @@ class MovieTestCase(TestCase):
         self.assertEqual(movie_in.age_rating, movie.age_rating)
         self.assertEqual(movie_in.subtitles, movie.subtitles)
         self.assertEqual(movie_in.url, movie.url)
+
+    def test_movie_can_be_update_from_update_schema(self) -> None:
+        movie = Movie(
+            slug="some slug",
+            name="some name",
+            description="some description",
+            rating="100",
+            age_rating="18+",
+            subtitles="ENG",
+            url="https://example.com",
+        )
+
+        movie_in = MovieUpdate(
+            name="new name",
+            description="new description",
+            rating="9.5",
+            age_rating="16+",
+            subtitles="RU",
+            url="https://new-example.com",
+        )
+
+        for field_name, value in movie_in:
+            setattr(movie, field_name, value)
+
+        self.assertEqual(movie_in.name, movie.name)
+        self.assertEqual(movie_in.description, movie.description)
+        self.assertEqual(movie_in.rating, movie.rating)
+        self.assertEqual(movie_in.age_rating, movie.age_rating)
+        self.assertEqual(movie_in.subtitles, movie.subtitles)
+        self.assertEqual(movie_in.url, movie.url)
+
+    def test_movie_can_be_from_update_partial_schema(self) -> None:
+        movie_in = MoviePartialUpdate(
+            description="another some description",
+        )
+        movie = Movie(
+            slug="some slug",
+            name="new name",
+            description="new description",
+            rating="9.5",
+            age_rating="16+",
+            subtitles="RU",
+            url="https://new-example.com",
+        )
+        for field_name, value in movie_in.model_dump(exclude_unset=True).items():
+            setattr(movie, field_name, value)
+        self.assertEqual(movie.name, movie.name)
+        self.assertEqual(movie.description, movie.description)
+        self.assertEqual(movie.rating, movie.rating)
+        self.assertEqual(movie.age_rating, movie.age_rating)
+        self.assertEqual(movie.subtitles, movie.subtitles)
+        self.assertEqual(movie.url, movie.url)
