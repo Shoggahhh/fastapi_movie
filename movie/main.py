25030f0d9aa98bf311bf9f2ab9e357c08fb27e82
vasiliy_grinev <vass_stn@mail.ru>
2025-09-04 23:32:53 +0300

    moved read_root to main_views.py

--- movie/main.py
+++ movie/main.py
@@ -1,8 +1,9 @@
 import logging
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 
 from api import router as api_router
+from api.main_views import router as main_router
 from api.redirect_views import router as redirect_router
 from app_lifespan import lifespan
 from core import config
@@ -14,17 +15,9 @@ app = FastAPI(
 
 app.include_router(redirect_router)
 app.include_router(api_router)
+app.include_router(main_router)
 
 logging.basicConfig(
     level=config.LOG_LEVEL,
     format=config.LOG_FORMAT,
 )
-
-
-@app.get("/")
-def read_root(request: Request) -> dict[str, str]:
-    docs_url = request.url.replace(
-        path="/docs",
-        query="",
-    )
-    return {"docs": str(docs_url)}
