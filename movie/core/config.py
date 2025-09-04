03e9395a1b4b7e9977cb2b94687024057d01f633
vasiliy_grinev <vass_stn@mail.ru>
2025-09-04 16:22:30 +0300

    fix

--- movie/core/config.py
+++ movie/core/config.py
@@ -12,7 +12,7 @@ LOG_FORMAT: str = (
 )
 
 REDIS_HOST = "localhost"
-REDIS_PORT = int(getenv("REDIS_PORT", 0)) or 6379  # noqa: PLW1508
+REDIS_PORT = int(getenv("REDIS_PORT", "0")) or 6379
 REDIS_DB = 0
 REDIS_DB_TOKENS = 1
 REDIS_DB_USER = 2
