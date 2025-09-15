5e0d9435ae82c68f9d80a5d844b3eb932a6e332d
vasiliy_grinev <vass_stn@mail.ru>
2025-09-15 17:35:08 +0300

    update check testing env

--- movie/testing/conftest.py
+++ movie/testing/conftest.py
@@ -8,17 +8,24 @@ import pytest
 from api.api_v1.movie.crud import storage
 from schemas.movie import Movie, MovieCreate
 
-if getenv("TESTING") != "1":
-    pytest.exit("Environment is not ready to testing")
+
+@pytest.fixture(
+    scope="session",
+    autouse=True,
+)
+def check_testing_env() -> None:
+    if getenv("TESTING") != "1":
+        pytest.exit("Environment is not ready to testing")
 
 
 def build_movie_create(
     slug: str,
+    name: str = "some name",
     description: str = "A movie",
 ) -> MovieCreate:
     return MovieCreate(
         slug=slug,
-        name="some name",
+        name=name,
         description=description,
         rating="100",
         age_rating="18+",
@@ -27,7 +34,10 @@ def build_movie_create(
     )
 
 
-def build_movie_create_random_slug() -> MovieCreate:
+def build_movie_create_random_slug(
+    name: str = "some name",
+    description: str = "A movie",
+) -> MovieCreate:
     return build_movie_create(
         slug="".join(
             random.choices(
@@ -35,22 +45,32 @@ def build_movie_create_random_slug() -> MovieCreate:
                 k=8,
             ),
         ),
+        name=name,
+        description=description,
     )
 
 
 def create_movie(
     slug: str,
+    name: str = "some name",
     description: str = "A movie",
 ) -> Movie:
     movie_in = build_movie_create(
         slug=slug,
+        name=name,
         description=description,
     )
     return storage.create(movie_in)
 
 
-def create_movie_random_slug() -> Movie:
-    movie_in = build_movie_create_random_slug()
+def create_movie_random_slug(
+    name: str = "some name",
+    description: str = "A movie",
+) -> Movie:
+    movie_in = build_movie_create_random_slug(
+        name=name,
+        description=description,
+    )
     return storage.create(movie_in)
 
 
