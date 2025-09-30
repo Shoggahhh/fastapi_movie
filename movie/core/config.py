1c47aa89ba6196af64eb830ecd4d03b4f08f0f9d
vasiliy_grinev <vass_stn@mail.ru>
2025-10-01 01:51:18 +0300

    update config

--- movie/core/config.py
+++ movie/core/config.py
@@ -1,22 +1,49 @@
 import logging
-from os import getenv
 from pathlib import Path
 
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+
 BASE_DIR = Path(__file__).resolve().parent.parent
-MOVIE_STORAGE_DIR = BASE_DIR / "movie.json"
 
-
-LOG_LEVEL = logging.INFO
 LOG_FORMAT: str = (
     "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
 )
 
-REDIS_HOST = getenv("REDIS_HOST", "localhost")
-REDIS_PORT = int(getenv("REDIS_PORT", "0")) or 6379
-REDIS_DB = 0
-REDIS_DB_TOKENS = 1
-REDIS_DB_USER = 2
-REDIS_DB_MOVIE = 3
 
-REDIS_TOKENS_SET_NAME = "tokens"
-REDIS_MOVIES_HASH_NAME = "movies"
+class LoggingConfig(BaseModel):
+    log_level: int = logging.INFO
+    log_format: str = LOG_FORMAT
+    date_format: str = "%Y-%m-%d %H:%M:%S"
+
+
+class RedisDataBaseConfig(BaseModel):
+    default: int = 0
+    tokens: int = 1
+    users: int = 2
+    movie: int = 3
+
+
+class RedisCollectionNames(BaseModel):
+    tokens_set: str = "tokens"
+    movie_hash: str = "movies"
+
+
+class RedisConnectionConfig(BaseModel):
+    host: str = "localhost"
+    port: int = 6379
+
+
+class RedisConfig(BaseModel):
+    connection: RedisConnectionConfig = RedisConnectionConfig()
+    db: RedisDataBaseConfig = RedisDataBaseConfig()
+    collections_name: RedisCollectionNames = RedisCollectionNames()
+
+
+class Settings(BaseSettings):
+    logging: LoggingConfig = LoggingConfig()
+    redis: RedisConfig = RedisConfig()
+
+
+# noinspection PyArgumentList
+settings = Settings()
